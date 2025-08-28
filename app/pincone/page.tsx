49239371c596adea22414e8484fd6ce6d1f5770d
Akarsh Singh <singhakarsh90@gmail.com>
2025-08-29 01:36:42 +0530

    Create Update KnowledgeBase Frontend

---
+++ app/pincone/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
+import { Database, LucideLoader2, MoveUp, RefreshCcw } from 'lucide-react';
+import React, { useState } from 'react';
+
+type Props = {
+   
+}
+
+function VectorDBPage(props: Props) {
+   const [isUploading,setIsUploading] = useState(false);
+   const [indexName,setIndexName] = useState("");
+   const [namespace,setNameSpace] = useState("");
+
+   const onStartUpload = async () => {
+     const response = await fetch('api/updatedatabase',{
+      method: 'POST',
+      body: JSON.stringify({
+        indexName,
+        namespace
+      })
+     });
+     console.log(response);
+
+     // await processStreamedProgress(response);
+   }
+
+   return (
+      <main className="flex flex-col items-center p-24">
+        <Card>
+         <CardHeader>
+           <CardTitle>Update Knowledge Base</CardTitle>
+           <CardDescription>Add new documents to your vector DB</CardDescription>
+         </CardHeader>
+         <CardContent>
+            <div className='grid grid-cols-3 gap-4'>
+              <div className='col-span-2 grid border rounded-lg p-6'>
+                <div className='gap-4 relative'>
+                   <Button onClick={onStartUpload} className='absolute -right-4 -top-4 ' variant={'ghost'} size={'icon'}>
+                     <RefreshCcw/>
+                   </Button>
+                   <Label>Files List</Label>
+                   <Textarea readOnly className='min-h-24 resize-none border p-3 shadow-none disabled:cursor-default focus-visible:ring-0 text-sm text-muted-foreground'/>
+                </div>
+                <div className='grid grid-cols-2 gap-4'>
+                  <div className="grid gap-2">
+                     <Label>Index Name</Label>
+                     <Input value={indexName} onChange={e => setIndexName(e.target.value)} placeholder='index name' disabled={isUploading} className='disabled:cursor-default'/>
+                  </div>
+                  <div className="grid gap-2">
+                     <Label>Namespace</Label>
+                     <Input value={namespace} onChange={e => setNameSpace(e.target.value)} placeholder='namespace' disabled={isUploading} className='disabled:cursor-default'/>                     
+                  </div>
+                </div>
+              </div>
+              <Button variant={'outline'} className='h-full w-full' disabled={isUploading}>
+                <span className='flex'>
+                 <Database className='size-12 stroke-[#D90013]'/>
+                 <MoveUp className='stroke-[#D90013]'/>
+                </span>
+              </Button>
+            </div>
+
+            {isUploading && <div className='mt-4'>
+              <Label>File Name:</Label>
+              <div className='flex items-center gap-4'>
+                <Progress value={80}/>
+                <LucideLoader2 className='stroke-[#D90013] animate-spin'/>
+              </div>
+            </div>}
+
+         </CardContent>
+        </Card>
+      </main>
+   )
+}
+
+export default VectorDBPage;
